c9ba5059b54925cf1ef01ed926780b3a6e838a36
Fred Bessette <msfredb7@gmail.com>
2017-05-04 17:11:01 -0400

    CamAdjustement

---
+++ ChatMage/Assets/Game/Prototypage/Scripts/CamAdjustment.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamAdjustment : MonoBehaviour {
+    public Camera cam;
+
+    void Start ()
+    {
+        float ratio = cam.aspect;
+        print("aspect: " + ratio);
+        transform.position = new Vector3(cam.orthographicSize * ratio, cam.orthographicSize, -100);
+	}
+}
---
+++ ChatMage/Assets/Game/Prototypage/Scripts/TestController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestController : MonoBehaviour
+{
+    public enum ControlMode { Standard }
+    public Tapis tapis;
+    public ControlMode mode;
+
+    void Update()
+    {
+        //if(Input.tou)
+    }
+
+}
