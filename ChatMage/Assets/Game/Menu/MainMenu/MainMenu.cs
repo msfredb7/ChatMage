27c5c3be48beecd1400ef9cc6122753bf3788e58
DESKTOP-KF9CH14\Lonely Boy Studio <alexandre.blanchet.94@gmail.com>
2017-05-17 11:28:39 -0400

    Debut des menu, ajout d'un spawner generique.

---
+++ ChatMage/Assets/Game/Menu/MainMenu/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using CCC.Manager;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	public void LoadScene(string name)
+    {
+        Scenes.Load(name);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
