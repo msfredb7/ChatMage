17c3a2130371f74597c7e3090edf3b6d10bfac37
Alexandre <alexandre.blanchet.94@gmail.com>
2017-05-17 17:52:39 -0400

    Second push

---
+++ ChatMage/Assets/Game/Level/demoLevelScript.cs
@@ -0,0 +1,40 @@
+﻿using CCC.Manager;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+using FullSerializer;
+
+public class demoLevelScript : LevelScript
+{
+    Unit myUnit;
+
+    //TRES IMPORTANT DE RESET NOS VARIABLE ICI
+    public override void OnGameReady()
+    {
+        myUnit = ResourceLoader.LoadEnemy("Enemy");
+    }
+
+    public override void OnGameStarted()
+    {
+
+    }
+
+    public override void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isOver)
+            {
+                isOver = true;
+                onObjectiveComplete.Invoke();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Game.instance.spawner.SpawnUnitAtRandomLocation(myUnit, Waypoint.WaypointType.enemySpawn);
+        }
+    }
+}
