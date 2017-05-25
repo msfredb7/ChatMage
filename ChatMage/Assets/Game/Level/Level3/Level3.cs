1aa0cbe9134ffba4e7023481ad767eda283401d3
Alexandre <alexandre.blanchet.94@gmail.com>
2017-05-25 11:41:07 -0400

    Ajout du niveau 3 et du dodger

---
+++ ChatMage/Assets/Game/Level/Level3/Level3.cs
@@ -0,0 +1,74 @@
+﻿using CCC.Manager;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+using FullSerializer;
+using FullInspector;
+
+public class Level3 : LevelScript {
+
+    public float enemySpawnDelay = 4f;
+    public float hpSpawnDelay = 8f;
+
+    [fsIgnore]
+    Unit dodger;
+    [fsIgnore]
+    Unit healthPacks;
+
+    //TRES IMPORTANT DE RESET NOS VARIABLE ICI
+    protected override void OnGameReady()
+    {
+        events.LockPlayer();
+
+        events.EndGameEvent(20);
+
+        events.IntroCountdown();
+    }
+
+    protected override void OnGameStarted()
+    {
+        events.UnLockPlayer();
+
+        events.SpawnEntitySpreadTime(dodger, 20, Waypoint.WaypointType.enemySpawn, 10, true);
+        events.SpawnEntitySpreadTime(healthPacks, 20, Waypoint.WaypointType.enemySpawn, 5, true);
+    }
+
+    protected override void OnUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !LoadingScreen.IsInTransition)
+        {
+            if (!isOver)
+            {
+                isOver = true;
+                onObjectiveComplete.Invoke();
+            }
+        }
+    }
+
+    protected override void OnEnd()
+    {
+        if (isOver)
+            return;
+        isOver = true;
+        
+    }
+
+    public override void OnInit(Action onComplete)
+    {
+        LoadQueue queue = new LoadQueue(onComplete);
+        queue.AddEnemy("Dodger", (x) => dodger = x);
+        queue.AddMiscUnit("HealthPacks", (x) => healthPacks = x);
+    }
+
+    public override void ReceiveEvent(string message)
+    {
+        switch (message)
+        {
+            default:
+                Debug.LogWarning("Demo level script received an unhandled event: " + message);
+                break;
+        }
+    }
+}
---
+++ ChatMage/Assets/Game/Units/Enemies/Dodger/DodgerBrain.cs
@@ -0,0 +1,60 @@
+﻿using CCC.Manager;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DodgerBrain : EnemyBrain<DodgerVehicle>
+{
+    public float minDodgeDelay = 1;
+    public float maxDodgeDelay = 2;
+
+    public bool changeDirection;
+    public enum Direction
+    {
+        right = 0,
+        left = 1
+    }
+
+    public Direction currentDirection;
+
+    protected override void Start()
+    {
+        base.Start();
+        vehicle.Init();
+    }
+
+    public void Update()
+    {
+        vehicle.LookAtPlayer();
+
+        if (changeDirection)
+            ChangeDirection();
+
+        if(currentDirection == 0)
+        {
+            vehicle.DodgeRight();
+        } else
+        {
+            vehicle.DodgeLeft();
+        }
+    }
+
+    void ChangeDirection()
+    {
+        switch (currentDirection)
+        {
+            case Direction.right:
+                currentDirection = Direction.left;
+                break;
+            case Direction.left:
+                currentDirection = Direction.right;
+                break;
+        }
+        changeDirection = false;
+        DelayManager.CallTo(delegate ()
+        {
+            changeDirection = true;
+        }, Random.Range(minDodgeDelay, maxDodgeDelay));
+
+    }
+}
