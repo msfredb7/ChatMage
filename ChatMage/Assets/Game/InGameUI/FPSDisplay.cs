c0c1508725fb5ae5286eb9d75a3fafabbe30f235
Alexandre <alexandre.blanchet.94@gmail.com>
2017-05-26 14:48:09 -0400

    Couple de changement au UI dont le FPS counter

---
+++ ChatMage/Assets/Game/InGameUI/FPSDisplay.cs
@@ -0,0 +1,13 @@
+﻿using CCC.Manager;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FPSDisplay : BaseManager<FPSDisplay> {
+
+    public override void Init()
+    {
+        Debug.Log("FPS init");
+    }
+}
