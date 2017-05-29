617a51e7cb317f32fee925378080d533df40dda5
Alexandre <alexandre.blanchet.94@gmail.com>
2017-05-29 11:36:57 -0400

    Changement du Loadout pour qu'il soit utilisable

--- ChatMage/Assets/Game/Menus/Loadout/Loadout.cs
+++ ChatMage/Assets/Game/Menus/Loadout/Loadout.cs
@@ -3,12 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Loadout : MonoBehaviour
 {
     public Armory armory;
 
     private LevelScript currentLevel;
+
+    public GameObject button;
+
+    public GameObject itemButtonsCountainer;
+    public GameObject smashButtonsCountainer;
+    public GameObject carButtonsCountainer;
+
+    public LoadoutResult currentLoadout;
 
     public class LoadoutMessage : SceneMessage
     {
@@ -36,14 +45,50 @@ public class Loadout : MonoBehaviour
     public void Init(LevelScript level)
     {
         currentLevel = level;
+        armory.Load();
+        armory.DebugSetItemSlot(5);
+        currentLoadout = new LoadoutResult(armory.GetItemSlots());
+
+        List<EquipablePreview> unlockItems = armory.GetAllUnlockedItems();
+        List<EquipablePreview> unlockCars = armory.GetAllUnlockedCars();
+        List<EquipablePreview> unlockSmashes = armory.GetAllUnlockedSmash();
+
+        for (int i = 0; i < unlockItems.Count; i++)
+        {
+            EquipablePreview currentEquipable = unlockItems[i];
+            GameObject newButton = Instantiate(button, itemButtonsCountainer.transform);
+            newButton.GetComponent<LoadoutButton>().ChangeLoadoutButton(currentEquipable.displayName);
+            newButton.GetComponent<Button>().onClick.AddListener(delegate() {
+                currentLoadout.AddEquipable(currentEquipable.equipableAssetName, currentEquipable.type);
+                Debug.Log(currentEquipable.displayName + " added");
+            });
+        }
+
+        for (int i = 0; i < unlockCars.Count; i++)
+        {
+            EquipablePreview currentEquipable = unlockCars[i];
+            GameObject newButton = Instantiate(button, carButtonsCountainer.transform);
+            newButton.GetComponent<LoadoutButton>().ChangeLoadoutButton(currentEquipable.displayName);
+            newButton.GetComponent<Button>().onClick.AddListener(delegate () {
+                currentLoadout.AddEquipable(currentEquipable.equipableAssetName, currentEquipable.type);
+                Debug.Log(currentEquipable.displayName + " selected");
+            });
+        }
+
+        for (int i = 0; i < unlockSmashes.Count; i++)
+        {
+            EquipablePreview currentEquipable = unlockSmashes[i];
+            GameObject newButton = Instantiate(button, smashButtonsCountainer.transform);
+            newButton.GetComponent<LoadoutButton>().ChangeLoadoutButton(currentEquipable.displayName);
+            newButton.GetComponent<Button>().onClick.AddListener(delegate () {
+                currentLoadout.AddEquipable(currentEquipable.equipableAssetName, currentEquipable.type);
+                Debug.Log(currentEquipable.displayName + " selected");
+            });
+        }
     }
 
-    public void TestClick()
+    public void ChargeLoadoutAndGame()
     {
-        LoadoutResult loadoutResult = new LoadoutResult();
-        loadoutResult.AddEquipable(armory.DebugGetCar().equipableAssetName, armory.DebugGetCar().type);
-        loadoutResult.AddEquipable(armory.DebugGetSmash().equipableAssetName, armory.DebugGetSmash().type);
-        loadoutResult.AddEquipable(armory.DebugGetItem().equipableAssetName, armory.DebugGetItem().type);
-        LoadingScreen.TransitionTo("Framework", new LoadoutMessage(currentLevel, loadoutResult), false);
+        LoadingScreen.TransitionTo("Framework", new LoadoutMessage(currentLevel, currentLoadout), false);
     }
 }
---
+++ ChatMage/Assets/Game/Menus/Loadout/LoadoutButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadoutButton : MonoBehaviour {
+
+    public Text uiText;
+
+    public void ChangeLoadoutButton(string text)
+    {
+        uiText.text = text;
+    }
+}
