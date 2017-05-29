2228af5df3ecd0dbead29f6dd90e37acc38a289f
Alexandre <alexandre.blanchet.94@gmail.com>
2017-05-29 12:48:44 -0400

    Debut du rocket

---
+++ ChatMage/Assets/Game/Equipables/Cars/Car_Rocket.cs
@@ -0,0 +1,37 @@
+﻿using FullSerializer;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car_Rocket : Car
+{
+
+    //NE PAS MODIFIER IN-GAME
+    public float acceleration = 0;
+
+    public override void OnInputUpdate(float horizontalInput)
+    {
+        if(horizontalInput == 0)
+        {
+            player.vehicle.canMove.Unlock("rocket");
+        } else
+        {
+            player.vehicle.canMove.Lock("rocket");
+        }
+    }
+
+    public override void OnGameReady()
+    {
+
+    }
+
+    public override void OnGameStarted()
+    {
+
+    }
+
+    public override void OnUpdate()
+    {
+
+    }
+}
