dfcd5f0a7e458d559661e3340fb3c2308b5a45b5
Fred Bessette <msfredb7@gmail.com>
2017-05-29 21:07:29 -0400

    Rocket v2

--- ChatMage/Assets/Game/Equipables/Cars/Car_RocketV2.cs
+++ ChatMage/Assets/Game/Equipables/Cars/Car_RocketV2.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using FullSerializer;
 using CCC.Utility;
 
-public class Car_Demo : Car
+public class Car_RocketV2 : Car
 {
     //NE PAS MODIFIER IN-GAME
     public float turnClutch = 0;
@@ -18,12 +18,41 @@ public class Car_Demo : Car
     [fsIgnore]
     float lastHorizontal = 0;
 
+    private bool wasTurning = false;
+    private float charge = 0;
+
     public override void OnInputUpdate(float horizontalInput)
     {
         if (horizontalInput == 0 || !player.playerStats.canTurn)
+        {
+            //ICI
+            if (wasTurning)
+            {
+                // boost !
+                player.vehicle.canAccelerate.Unlock("car");
+                player.vehicle.Speed = player.vehicle.WorldDirection2D() * moveSpeed * Mathf.Min(charge, 4f);
+                charge = 0.5f;
+                player.vehicle.rb.drag = 0;
+                wasTurning = false;
+            }
+            //A ICI
+
             horizontal = 0;
+        }
         else
         {
+            //ICI
+            if (!wasTurning)
+            {
+                //disable acc
+                player.vehicle.canAccelerate.Lock("car");
+                player.vehicle.rb.drag = 1;
+
+                wasTurning = true;
+            }
+            //A ICI
+
+
             if ((horizontalInput < 0 && lastHorizontal > 0) || (horizontalInput > 0 && lastHorizontal < 0))
                 lastHorizontal = 0;
 
@@ -31,11 +60,19 @@ public class Car_Demo : Car
                 lastHorizontal = horizontalInput * turnClutch;
 
             horizontal = Mathf.MoveTowards(lastHorizontal, horizontalInput, player.vehicle.DeltaTime() * turnAcceleration);
+            
+
+            //ICI
+            charge += player.vehicle.DeltaTime() * 2;
+            //A ICI
         }
 
         player.vehicle.Rotation += -horizontal * turnSpeed * player.vehicle.DeltaTime();
 
         lastHorizontal = horizontal;
+
+
+
     }
 
     public override void OnGameReady()
