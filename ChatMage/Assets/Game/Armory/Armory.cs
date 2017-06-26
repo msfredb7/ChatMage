c2fdf57d856a874aa26bbba9310c5c5f4eb28c15
Fred Bessette <msfredb7@gmail.com>
2017-06-26 16:27:16 -0400

    Menage d'armory

--- ChatMage/Assets/Game/Armory/Armory.cs
+++ ChatMage/Assets/Game/Armory/Armory.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Armory : ScriptableObject
+public class Armory : BaseScriptableObject
 {
     private const string SLOTS_KEY = "slt";
     // On a des previews qu'on affichera dans le UI pour ensuite 
