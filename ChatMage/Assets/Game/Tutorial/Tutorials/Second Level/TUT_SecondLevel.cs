c3fde3ef42206308952ffdc6be55b88066f48749
Xeal <alexandre-blanchet94@hotmail.com>
2017-07-11 14:06:24 -0400

    Level 2

---
+++ ChatMage/Assets/Game/Tutorial/Tutorials/Second Level/TUT_SecondLevel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Tutorial;
+using UnityEngine;
+
+namespace Tutorial
+{
+    public class TUT_SecondLevel : BaseTutorial
+    {
+        protected override void Cleanup()
+        {
+        }
+
+        protected override void OnStart()
+        {
+        }
+    }
+}
+
