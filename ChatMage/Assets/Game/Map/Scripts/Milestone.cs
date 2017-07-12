ffac9f9ad0cc06dba14136cb479e071c89594f21
Fred Bessette <msfredb7@gmail.com>
2017-07-12 12:42:11 -0400

    Gizmos

--- ChatMage/Assets/Game/Map/Scripts/Milestone.cs
+++ ChatMage/Assets/Game/Map/Scripts/Milestone.cs
@@ -10,22 +10,28 @@ public class Milestone : BaseBehavior
     public TriggerType triggerOn;
     public bool disapearAfterTrigger = true;
 
+    [InspectorMargin(12), InspectorHeader("AI Area")]
+    public bool setAiArea;
+    [InspectorShowIf("setAiArea")]
+    public bool aiAreaAdjustToSceenRatio = true;
+    [InspectorShowIf("setAiArea")]
+    public Box2D aiAreaRelativeToMilestone;
 
-    [InspectorHeader("Map Stop")]
+    [InspectorMargin(12), InspectorHeader("Map Stop")]
     public bool modifyFollowPlayer;
     [InspectorShowIf("modifyFollowPlayer")]
     public bool followPlayerEffect = false;
     [InspectorMargin(5)]
-    public bool modifyCanScrollUp = false;
-    [InspectorShowIf("modifyCanScrollUp")]
-    public bool canScrollUpEffect = true;
+    public bool setCameraMin = false;
+    [InspectorShowIf("setCameraMin")]
+    public float cameraMinRelativeToMilestone = 0;
     [InspectorMargin(5)]
-    public bool modifyCanScrollDown = false;
-    [InspectorShowIf("modifyCanScrollDown")]
-    public bool canScrollDownEffect = true;
+    public bool setCameraMax = false;
+    [InspectorShowIf("setCameraMax")]
+    public float cameraMaxRelativeToMilestone = 0;
 
 
-    [InspectorHeader("Event")]
+    [InspectorMargin(12), InspectorHeader("Event")]
     public bool fireEventToLevelScript;
     [InspectorShowIf("fireEventToLevelScript")]
     public string eventMessage;
@@ -35,17 +41,32 @@ public class Milestone : BaseBehavior
     {
         if (!gameObject.activeSelf)
             return false;
-        if (modifyCanScrollDown)
+
+        if (setAiArea)
         {
-            Game.instance.gameCamera.canScrollDown = canScrollDownEffect;
+            Vector2 min = aiAreaRelativeToMilestone.min + (Vector2)transform.position;
+            Vector2 max = aiAreaRelativeToMilestone.max + (Vector2)transform.position;
+            if (aiAreaAdjustToSceenRatio)
+            {
+                GameCamera cam = Game.instance.gameCamera;
+                min = cam.AdjustVector(min);
+                max = cam.AdjustVector(max);
+            }
+            Game.instance.aiArea.SetArea(min, max);
         }
-        if (modifyCanScrollUp)
+
+        if (setCameraMax)
         {
-            Game.instance.gameCamera.canScrollUp = canScrollUpEffect;
+            Game.instance.gameCamera.maxHeight = transform.position.y + cameraMaxRelativeToMilestone;
         }
+        if (setCameraMin)
+        {
+            Game.instance.gameCamera.minHeight = transform.position.y + cameraMinRelativeToMilestone;
+        }
+
         if (modifyFollowPlayer)
         {
-            Game.instance.gameCamera.followPlayer = canScrollDownEffect;
+            Game.instance.gameCamera.followPlayer = followPlayerEffect;
         }
 
         if (fireEventToLevelScript)
@@ -64,5 +85,38 @@ public class Milestone : BaseBehavior
     {
         Gizmos.color = new Color(triggerOn == TriggerType.BottomOfScreen ? 1 : 0, triggerOn == TriggerType.TopOfScreen ? 1 : 0, 0, 1);
         Gizmos.DrawCube(transform.position, new Vector3(16, disapearAfterTrigger ? 0.25f : 0.5f, 1));
+
+
+        if (modifyFollowPlayer)
+        {
+            Vector3 pos = transform.position + Vector3.left * 8;
+            if (followPlayerEffect)
+            {
+                Gizmos.DrawIcon(pos, "Gizmos CameraFollow");
+            }
+            else
+            {
+                Gizmos.DrawIcon(pos, "Gizmos CameraStopFollow");
+            }
+        }
+
+        if (setAiArea)
+        {
+            Gizmos.color = new Color(0.2f, 1, 0, 0.4f);
+            Gizmos.DrawCube((Vector2)transform.position + aiAreaRelativeToMilestone.Center, aiAreaRelativeToMilestone.Size);
+        }
+
+        if (setCameraMax)
+        {
+            float delta = (triggerOn == TriggerType.BottomOfScreen) ? 9 : 0;
+            Vector3 pos = transform.position + Vector3.up * (cameraMaxRelativeToMilestone + delta);
+            Gizmos.DrawIcon(pos, "Gizmos CameraTop");
+        }
+        if (setCameraMin)
+        {
+            float delta = (triggerOn == TriggerType.BottomOfScreen) ? 0 : -9;
+            Vector3 pos = transform.position + Vector3.up * (cameraMinRelativeToMilestone + delta);
+            Gizmos.DrawIcon(pos, "Gizmos CameraBottom");
+        }
     }
 }
