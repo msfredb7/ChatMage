adc063a1363ce889ef3f0768162eb4b16c6a83db
Fred Bessette <msfredb7@gmail.com>
2017-07-25 15:10:15 -0400

    Smash shield tweaks

--- ChatMage/Assets/Game/Equipables/Items/SmashShield/ITM_SmashShield.cs
+++ ChatMage/Assets/Game/Equipables/Items/SmashShield/ITM_SmashShield.cs
@@ -1,53 +1,89 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ITM_SmashShield : Item {
+public class ITM_SmashShield : Item, IAttackable
+{
 
     public GameObject shieldPrefab;
-    private GameObject shield;
+    public float maxShieldDuration = 10;
 
-    public float limitCooldown = 10;
-    private float counter;
-    private bool startCounter;
+    [NonSerialized, FullSerializer.fsIgnore]
+    private GameObject shieldInstance;
+    private float remainingShieldDuration;
+
+    public override void Init(PlayerController player)
+    {
+        base.Init(player);
+
+        //Spawn shield
+        shieldInstance = Instantiate(shieldPrefab, player.body);
+    }
 
     public override void OnGameReady()
     {
-        startCounter = false;
-        counter = 0;
+        //Shield off on start
+        ShieldOff();
     }
 
     public override void OnGameStarted()
     {
+        //Set listeners
         player.playerSmash.onSmashGained += PlayerSmash_onSmashGained;
         player.playerSmash.onSmashStarted += PlayerSmash_onSmashStarted;
     }
 
     private void PlayerSmash_onSmashGained()
     {
-        shield = Instantiate(shieldPrefab,player.body.transform);
-        player.playerStats.damagable = false;
-
-        startCounter = true;
-        counter = limitCooldown;
+        ShieldOn();
     }
 
     private void PlayerSmash_onSmashStarted()
     {
-        Destroy(shield);
-        player.playerStats.damagable = true;
-
-        startCounter = false;
-        counter = 0;
+        ShieldOff();
     }
 
     public override void OnUpdate()
     {
-        if(counter < 0 && startCounter)
+        bool wasOn = IsShieldOn();
+
+        remainingShieldDuration -= player.vehicle.DeltaTime();
+
+        if (!IsShieldOn() && wasOn)
         {
-            PlayerSmash_onSmashStarted();
+            ShieldOff();
         }
-        counter -= player.vehicle.DeltaTime();
+    }
+
+    private void ShieldOn()
+    {
+        shieldInstance.SetActive(true);
+        remainingShieldDuration = maxShieldDuration;
+    }
+    private void ShieldOff()
+    {
+        shieldInstance.SetActive(false);
+        remainingShieldDuration = -1;
+    }
+
+    protected override void ClearReferences()
+    {
+        base.ClearReferences();
+
+        shieldInstance = null;
+    }
+
+    private bool IsShieldOn()
+    {
+        return remainingShieldDuration > 0;
+    }
+
+    public int Attacked(ColliderInfo on, int amount, Unit otherUnit, ColliderInfo source = null)
+    {
+        if (IsShieldOn())
+            return 0;
+        else
+            return amount;
     }
 }
