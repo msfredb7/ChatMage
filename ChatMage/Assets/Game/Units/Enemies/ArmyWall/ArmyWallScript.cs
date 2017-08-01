b2b61ff767fd054a1ee0817761a8b6a324730ec0
Xeal <alexandre-blanchet94@hotmail.com>
2017-08-01 15:45:15 -0400

    Changement au damage que le ArmyWall fait

--- ChatMage/Assets/Game/Units/Enemies/ArmyWall/ArmyWallScript.cs
+++ ChatMage/Assets/Game/Units/Enemies/ArmyWall/ArmyWallScript.cs
@@ -7,20 +7,32 @@ public class ArmyWallScript : Unit {
     public float awayFromPlayer = 1f;
     public float nearPlayer = 1f;
 
+    public float bumpStrength;
+    public float bumpDuration = 0f;
+
     public SimpleColliderListener colliderListener;
     
     public bool marchinY = true;
     public bool beginMarching = false;
+
+    private bool justHitPlayer;
+    private float cooldown;
 	
     void Start()
     {
         colliderListener.onTriggerEnter += ColliderListener_onTriggerEnter;
+        justHitPlayer = false;
     }
 
     private void ColliderListener_onTriggerEnter(ColliderInfo other, ColliderListener listener)
     {
-        if(other.parentUnit is PlayerVehicle)
-            (other.parentUnit as PlayerVehicle).Kill();
+        if (other.parentUnit is PlayerVehicle)
+        {
+            Game.instance.Player.playerStats.Attacked(other, 1, this);
+            (other.parentUnit as PlayerVehicle).Bump(WorldDirection2D() * bumpStrength, bumpDuration, BumpMode.VelocityAdd);
+            justHitPlayer = true;
+            cooldown = 2; // Le ArmyWall recommence a marcher apres 2 secondes
+        }
         if (other.parentUnit is EnemyVehicle)
             (other.parentUnit as EnemyVehicle).ForceDie();
     }
@@ -34,18 +46,22 @@ public class ArmyWallScript : Unit {
             {
                 if (marchinY)
                 {
-                    if (Vector2.Distance(Game.instance.Player.vehicle.Position, transform.position) > 10)
+                    if (Vector2.Distance(Game.instance.Player.vehicle.Position, transform.position) > 10 || justHitPlayer)
                         transform.position = new Vector2(transform.position.x, transform.position.y + (Game.instance.Player.vehicle.DeltaTime() * awayFromPlayer)); // si hors du screen
                     else
                         transform.position = new Vector2(transform.position.x, transform.position.y + (Game.instance.Player.vehicle.DeltaTime() * nearPlayer)); // si inside screen
                 } else
                 {
-                    if (Vector2.Distance(Game.instance.Player.vehicle.Position, transform.position) > 10)
+                    if (Vector2.Distance(Game.instance.Player.vehicle.Position, transform.position) > 10 || justHitPlayer)
                         transform.position = new Vector2(transform.position.x + (Game.instance.Player.vehicle.DeltaTime() * awayFromPlayer), transform.position.y); // si hors du screen
                     else
                         transform.position = new Vector2(transform.position.x + (Game.instance.Player.vehicle.DeltaTime() * nearPlayer), transform.position.y); // si inside screen
                 }
             }
         }
+        if (justHitPlayer)
+            if (cooldown < 0)
+                justHitPlayer = false;
+        cooldown -= DeltaTime();
     }
 }
