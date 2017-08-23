87a2bbff601ea3e8f690394d4415f84af7eaf9dc
Xeal <alexandre-blanchet94@hotmail.com>
2017-08-22 21:09:55 -0400

    BIG UPDATE 2.0 LS_1_3 !!!!!

--- ChatMage/Assets/Game/Levels/Levels/Monde 1/Scripts/LS_ThridLevel.cs
+++ ChatMage/Assets/Game/Levels/Levels/Monde 1/Scripts/LS_ThridLevel.cs
@@ -19,33 +19,96 @@ public class LS_ThridLevel : LevelScript
     [fsIgnore, NonSerialized]
     private bool canWin = false;
 
-    private Unit archerArmy;
     private TaggedObject jesusWall;
     private TaggedObject grilleWall1;
     private TaggedObject grilleWall2;
+    private TaggedObject armyWall;
+
+    // Ambush 1
+    private List<Unit> soldatsAmbush1 = new List<Unit>();
+    private TaggedObject soldat1;
+    private TaggedObject soldat2;
+    private TaggedObject soldat3;
+    private TaggedObject soldat4;
+    private TaggedObject soldat5;
+
+    // Ambush 2
+    private List<Unit> soldatsAmbush2 = new List<Unit>();
+    private TaggedObject soldat6;
+    private TaggedObject soldat7;
+    private TaggedObject soldat8;
+    private TaggedObject soldat9;
+    private TaggedObject soldat0;
 
     protected override void ResetData()
     {
         base.ResetData();
+        soldatsAmbush1.Clear();
+        soldatsAmbush2.Clear();
         canWin = false;
     }
 
     protected override void OnGameReady()
     {
         map = Game.instance.map;
-        TaggedObject armyWall = map.mapping.GetTaggedObject("army wall");
+        
+        Game.instance.gameCamera.canScrollDown = true;
+
+        armyWall = map.mapping.GetTaggedObject("army wall");
+        armyWall.gameObject.SetActive(true);
         armyWall.gameObject.GetComponent<ArmyWallScript>().beginMarching = true;
+
+        inGameEvents.AddDelayedAction(delegate () { armyWall.gameObject.transform.position = new Vector3(-0.13f, -27.75f, 0); }, 2);
 
         grilleWall1 = map.mapping.GetTaggedObject("grille 1");
 
         grilleWall2 = map.mapping.GetTaggedObject("grille 2");
+
+        // Ambush 1 Enemies
+        soldat1 = map.mapping.GetTaggedObject("ambush 1 soldat 1");
+        soldatsAmbush1.Add(soldat1.GetComponent<Vehicle>());
+        soldat2 = map.mapping.GetTaggedObject("ambush 1 soldat 2");
+        soldatsAmbush1.Add(soldat2.GetComponent<Vehicle>());
+        soldat3 = map.mapping.GetTaggedObject("ambush 1 soldat 3");
+        soldatsAmbush1.Add(soldat3.GetComponent<Vehicle>());
+        soldat4 = map.mapping.GetTaggedObject("ambush 1 soldat 4");
+        soldatsAmbush1.Add(soldat4.GetComponent<Vehicle>());
+        soldat5 = map.mapping.GetTaggedObject("ambush 1 soldat 5");
+        soldatsAmbush1.Add(soldat5.GetComponent<Vehicle>());
+
+        // Ambush 2 Enemies
+        soldat6 = map.mapping.GetTaggedObject("ambush 2 soldat 1");
+        soldatsAmbush2.Add(soldat6.GetComponent<Vehicle>());
+        soldat7 = map.mapping.GetTaggedObject("ambush 2 soldat 2");
+        soldatsAmbush2.Add(soldat7.GetComponent<Vehicle>());
+        soldat8 = map.mapping.GetTaggedObject("ambush 2 soldat 3");
+        soldatsAmbush2.Add(soldat8.GetComponent<Vehicle>());
+        soldat9 = map.mapping.GetTaggedObject("ambush 2 soldat 4");
+        soldatsAmbush2.Add(soldat9.GetComponent<Vehicle>());
+        soldat0 = map.mapping.GetTaggedObject("ambush 2 soldat 5");
+        soldatsAmbush2.Add(soldat0.GetComponent<Vehicle>());
     }
 
     protected override void OnGameStarted()
     {
         Game.instance.ui.dialogDisplay.StartDialog(RUN);
+
         jesusWall = map.mapping.GetTaggedObject("jesus wall");
         jesusWall.gameObject.SetActive(false);
+
+        // Ambush 1
+        soldat1.GetComponent<Vehicle>().onDeath += OnFirstAmbushSoldierDeath;
+        soldat2.GetComponent<Vehicle>().onDeath += OnFirstAmbushSoldierDeath;
+        soldat3.GetComponent<Vehicle>().onDeath += OnFirstAmbushSoldierDeath;
+        soldat4.GetComponent<Vehicle>().onDeath += OnFirstAmbushSoldierDeath;
+        soldat5.GetComponent<Vehicle>().onDeath += OnFirstAmbushSoldierDeath;
+
+        // Ambush 2
+        soldat6.GetComponent<Vehicle>().onDeath += OnSecondAmbushSoldierDeath;
+        soldat7.GetComponent<Vehicle>().onDeath += OnSecondAmbushSoldierDeath;
+        soldat8.GetComponent<Vehicle>().onDeath += OnSecondAmbushSoldierDeath;
+        soldat9.GetComponent<Vehicle>().onDeath += OnSecondAmbushSoldierDeath;
+        soldat0.GetComponent<Vehicle>().onDeath += OnSecondAmbushSoldierDeath;
     }
 
     public void StartFirstWave()
@@ -70,7 +133,6 @@ public class LS_ThridLevel : LevelScript
     {
         Game.instance.ui.dialogDisplay.StartDialog(bossDialog, delegate ()
         {
-            jesusWall.gameObject.SetActive(true);
             TriggerWaveManually("jesus");
         });
     }
@@ -107,39 +169,42 @@ public class LS_ThridLevel : LevelScript
         switch (message)
         {
             case "spawn 1":
-                inGameEvents.AddDelayedAction(StartRoadAmbushOne, 0.5f);
+                //armyWall.gameObject.GetComponent<ArmyWallScript>().ForceDie(); marche pas ?
+                GameObject.Destroy(armyWall.gameObject); // marche !
+                Game.instance.units.Remove(armyWall.GetComponent<ArmyWallScript>());
+                // inGameEvents.AddDelayedAction(StartRoadAmbushOne, 0.5f); marche pas ? bug...
                 break;
             case "spawn 2":
                 inGameEvents.AddDelayedAction(StartRoadAmbushTwo, 0.5f);
                 break;
             case "first intersec":
-                inGameEvents.AddDelayedAction(StartFirstWave, 0.5f);
+                // Ambush 1
+                soldat1.GetComponent<Vehicle>().enabled = true;
+                soldat2.GetComponent<Vehicle>().enabled = true;
+                soldat3.GetComponent<Vehicle>().enabled = true;
+                soldat4.GetComponent<Vehicle>().enabled = true;
+                soldat5.GetComponent<Vehicle>().enabled = true;
+                inGameEvents.AddDelayedAction(StartFirstWave, 0.1f);
                 break;
             case "first intersec completed":
-                Game.instance.gameCamera.followPlayer = true;
-                Game.instance.gameCamera.canScrollUp = true;
-                grilleWall1.gameObject.SetActive(false); // animation grille
-                inGameEvents.AddDelayedAction(delegate () { archerArmy.ForceDie(); }, 1);
-                Game.instance.map.roadPlayer.CurrentRoad.ApplyMinMaxToCamera();
                 break;
             case "spawn 3":
-                inGameEvents.AddDelayedAction(StartRoadAmbushThree, 0.5f);
-                break;
-            case "spawn 4":
+                inGameEvents.AddDelayedAction(StartRoadAmbushThree, 0.1f);
                 inGameEvents.AddDelayedAction(StartRoadAmbushFour, 0.5f);
                 break;
             case "second intersec":
-                inGameEvents.AddDelayedAction(StartSecondWave, 0.5f);
+                // Ambush 2
+                soldat6.GetComponent<Vehicle>().enabled = true;
+                soldat7.GetComponent<Vehicle>().enabled = true;
+                soldat8.GetComponent<Vehicle>().enabled = true;
+                soldat9.GetComponent<Vehicle>().enabled = true;
+                soldat0.GetComponent<Vehicle>().enabled = true;
+                inGameEvents.AddDelayedAction(StartSecondWave, 0.1f);
                 break;
             case "second intersec completed":
-                Game.instance.gameCamera.followPlayer = true;
-                Game.instance.gameCamera.canScrollUp = true;
-                grilleWall2.gameObject.SetActive(false); // animation grille
-                inGameEvents.AddDelayedAction(delegate () { archerArmy.ForceDie(); }, 1);
-                Game.instance.map.roadPlayer.CurrentRoad.ApplyMinMaxToCamera();
-                canWin = true;
                 break;
             case "boss battle entry":
+                jesusWall.gameObject.SetActive(true);
                 LinkedListNode<Unit> node = Game.instance.units.First;
                 while (node != null)
                 {
@@ -153,7 +218,7 @@ public class LS_ThridLevel : LevelScript
                 }
                 break;
             case "boss battle":
-                inGameEvents.AddDelayedAction(StartBossWave, 0.5f);
+                inGameEvents.AddDelayedAction(StartBossWave, 0.1f);
                 canWin = true;
                 break;
             case "jesus dead":
@@ -163,10 +228,32 @@ public class LS_ThridLevel : LevelScript
         }
     }
 
-    public void FindArcherWall(Unit unit)
+    private void ResetRoad()
     {
-        if (unit is ArcherArmyUnit)
-            archerArmy = unit;
+        Game.instance.gameCamera.followPlayer = true;
+        Game.instance.gameCamera.canScrollUp = true;
+        Game.instance.map.roadPlayer.CurrentRoad.ApplyMinMaxToCamera();
+    }
+
+    private void OnFirstAmbushSoldierDeath(Unit unit)
+    {
+        soldatsAmbush1.Remove(unit);
+        if(soldatsAmbush1.Count < 1)
+        {
+            ResetRoad();
+            grilleWall1.gameObject.SetActive(false); // animation grille
+        }
+    }
+
+    private void OnSecondAmbushSoldierDeath(Unit unit)
+    {
+        soldatsAmbush2.Remove(unit);
+        if (soldatsAmbush2.Count < 1)
+        {
+            ResetRoad();
+            grilleWall2.gameObject.SetActive(false); // animation grille
+            canWin = true;
+        }
     }
 
     protected override void OnUpdate()
---
+++ ChatMage/Assets/Game/Units/Enemies/Antiquité/ArmyWall/StopSoldiers.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopSoldiers : MonoBehaviour {
+
+    private Vector3 startingPos;
+
+	void Start () {
+        startingPos = transform.localPosition;
+    }
+	
+	void Update ()
+    {
+        transform.localPosition = startingPos;
+    }
+}
