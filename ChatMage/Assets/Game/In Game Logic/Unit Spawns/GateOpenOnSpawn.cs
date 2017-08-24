6e1ea0d52928ed826373653340e96505726abec8
Fred Bessette <msfredb7@gmail.com>
2017-08-23 21:21:12 -0400

    Ajustements visuels du level 1 et 2 + Death animation fix de l'archer et du spearman

---
+++ ChatMage/Assets/Game/In Game Logic/Unit Spawns/GateOpenOnSpawn.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GateOpenOnSpawn : OnSpawnAction
+{
+    public UnityEvent open;
+    public UnityEvent close;
+    public float openTime = 1f;
+
+    private float timer = 0;
+    CCC.Utility.StatFloat worldTimeScale;
+
+    protected override void AttachedSpawn_onUnitSpawned(Unit unit)
+    {
+        if (unit == null)
+            return;
+
+        if (timer <= 0)
+            open.Invoke();
+
+        timer = openTime;
+    }
+
+    void Update()
+    {
+        if(timer > 0)
+        {
+            if(worldTimeScale == null)
+            {
+                worldTimeScale = Game.instance != null ? Game.instance.worldTimeScale : null;
+            }
+            else
+            {
+                timer -= worldTimeScale * Time.deltaTime;
+
+                if(timer <= 0)
+                {
+                    close.Invoke();
+                }
+            }
+        }
+    }
+}
--- ChatMage/Assets/Game/Levels/Levels/Monde 1/Scripts/LS_FirstLevel.cs
+++ ChatMage/Assets/Game/Levels/Levels/Monde 1/Scripts/LS_FirstLevel.cs
@@ -8,44 +8,9 @@ using FullSerializer;
 
 public class LS_FirstLevel : LevelScript
 {
-    [fsIgnore, NonSerialized]
-    private bool canWin = false;
-
-    protected override void ResetData()
-    {
-        base.ResetData();
-        canWin = false;
-    }
-
     protected override void OnGameReady()
     {
         Game.instance.smashManager.smashEnabled = false;
         Game.instance.ui.smashDisplay.canBeShown = false;
-    }
-
-    public override void OnReceiveEvent(string message)
-    {
-        switch (message)
-        {
-            case "final wave complete":
-                canWin = true;
-                break;
-            case "attempt win":
-                if (canWin)
-                    Win();
-                break;
-        }
-    }
-
-    protected override void OnUpdate()
-    {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            Win();
-        }
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            Lose();
-        }
     }
 }
