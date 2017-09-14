3d5a0bee8d67b4072a643a320fa3fafe8cf803b0
Frederic Bessette <msfredb7@gmail.com>
2017-09-13 21:09:17 -0400

    Tiled Sprite + Ajout visuels au lvl 1 EN COURS

--- ChatMage/Assets/TestScript.cs
+++ ChatMage/Assets/TestScript.cs
@@ -5,27 +5,16 @@ using System;
 using CCC.Manager;
 using CCC.Utility;
 using UnityEngine.UI;
+using DG.Tweening;
 using FullInspector;
 
 public class TestScript : MonoBehaviour
 {
-
-    void Awake()
-    {
-        Debug.LogWarning("Testscript here on " + gameObject.name + ". Don't forget me !");
-    }
-
-
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.T))
         {
-            Camera cam = Game.instance.gameCamera.cam;
-            cam.aspect = 16f / 9f;
-        }
-        if (Input.GetKeyDown(KeyCode.Y))
-        {
-
+            GetComponent<SpriteOffset>().DOOffset(Vector2.one, 4).SetLoops(-1);
         }
     }
 }
