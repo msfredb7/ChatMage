8e231b80c35b5fd0a2f254a13ccc767b37431da1
Fred Bessette <msfredb7@gmail.com>
2017-09-23 19:37:54 -0400

    J'ai tassé des dossier dans UI

---
+++ ChatMage/Assets/Game/UI/In Game/Dialog/Characters.cs
@@ -0,0 +1,190 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Dialoguing
+{
+    public class Characters : MonoBehaviour
+    {
+        public Image leftImage;
+        public Image rightImage;
+
+        // Special Entry
+        [HideInInspector]
+        public bool specialEntryMode = false;
+        [HideInInspector]
+        public bool supposeToShakeCharacter = false;
+
+        // Name
+        public GameObject leftName;
+        public GameObject rightName;
+
+        // Shaking
+        [HideInInspector]
+        public VectorShaker leftVectorShaker;
+        [HideInInspector]
+        public VectorShaker rightVectorShaker;
+        public float currentCharacterShakeIntensity = 50;
+        public float currentCharacterShakeDuration = 0.25f;
+
+        private Vector2 stdLeftOffset;
+        private Vector2 stdRightOffset;
+        private bool leftOffsetSet = false;
+        private bool rightOffsetSet = false;
+
+        void Awake()
+        {
+            stdLeftOffset = leftImage.rectTransform.anchoredPosition;
+            stdRightOffset = rightImage.rectTransform.anchoredPosition;
+
+            // Init Shake
+            specialEntryMode = false;
+            supposeToShakeCharacter = false;
+
+            // Init Shaking
+            leftVectorShaker = leftImage.GetComponent<VectorShaker>();
+            rightVectorShaker = rightImage.GetComponent<VectorShaker>();
+
+            HideBoth();
+        }
+
+        public void SetLeftImage(Sprite leftSprite)
+        {
+            if (leftOffsetSet)
+                ResetLeftOffset();
+
+            if (!specialEntryMode)
+                leftImage.enabled = true;
+            leftImage.sprite = leftSprite;
+
+            rightImage.enabled = false;
+        }
+        public void SetLeftImage(Sprite leftSprite, Vector2 offset)
+        {
+            SetLeftImage(leftSprite);
+            SetLeftOffset(offset);
+        }
+
+        public void SetRightImage(Sprite rightSprite)
+        {
+            if (rightOffsetSet)
+                ResetRightOffset();
+
+            if (!specialEntryMode)
+                rightImage.enabled = true;
+            rightImage.sprite = rightSprite;
+
+            leftImage.enabled = false;
+        }
+        public void SetRightImage(Sprite rightSprite, Vector2 offset)
+        {
+            SetRightImage(rightSprite);
+            SetRightOffset(offset);
+        }
+
+        public void SetBothImage(Sprite leftSprite, Sprite rightSprite)
+        {
+            if (rightOffsetSet)
+                ResetRightOffset();
+            if (leftOffsetSet)
+                ResetLeftOffset();
+
+            leftImage.enabled = true;
+            leftImage.sprite = leftSprite;
+            rightImage.enabled = true;
+            rightImage.sprite = rightSprite;
+        }
+        public void SetBothImage(Sprite leftSprite, Vector2 leftOffset, Sprite rightSprite, Vector2 rightOffset)
+        {
+            SetBothImage(leftSprite, rightSprite);
+            SetRightOffset(rightOffset);
+            SetLeftOffset(leftOffset);
+        }
+
+        public void HideBoth()
+        {
+            leftImage.enabled = false;
+            rightImage.enabled = false;
+        }
+
+        public void SetLeftOffset(Vector2 offset)
+        {
+            leftOffsetSet = true;
+            leftImage.rectTransform.anchoredPosition = stdLeftOffset + offset;
+        }
+
+        public void SetRightOffset(Vector2 offset)
+        {
+            rightOffsetSet = true;
+            rightImage.rectTransform.anchoredPosition = stdRightOffset + offset;
+        }
+
+        public void ResetLeftOffset()
+        {
+            leftOffsetSet = false;
+            leftImage.rectTransform.anchoredPosition = stdLeftOffset;
+        }
+
+        public void ResetRightOffset()
+        {
+            rightOffsetSet = false;
+            rightImage.rectTransform.anchoredPosition = stdRightOffset;
+        }
+
+        public void SetLeftCharacterShake(float strength, float duration)
+        {
+            leftVectorShaker.Shake(strength, duration);
+        }
+
+        public void SetRightCharacterShaker(float strength, float duration)
+        {
+            rightVectorShaker.Shake(strength, duration);
+        }
+
+        public void SetLeftText(string text)
+        {
+            DisableRightName();
+            ActivateLeftName(text);
+        }
+
+        public void SetRightText(string text)
+        {
+            DisableLeftName();
+            ActivateRightName(text);
+        }
+
+        public void DisableRightName()
+        {
+            Image myImage = rightImage.GetComponentInChildren<NameSetText>().GetComponent<Image>();
+            myImage.color = myImage.color.ChangedAlpha(0);
+            rightImage.GetComponentInChildren<NameSetText>().SetText("");
+        }
+
+        public void ActivateRightName(string text)
+        {
+            Image myImage = rightImage.GetComponentInChildren<NameSetText>().GetComponent<Image>();
+            myImage.color = myImage.color.ChangedAlpha(1);
+            rightImage.GetComponentInChildren<NameSetText>().SetText(text);
+            if (specialEntryMode)
+                myImage.gameObject.SetActive(false);
+        }
+
+        public void DisableLeftName()
+        {
+            Image myImage = leftImage.GetComponentInChildren<NameSetText>().GetComponent<Image>();
+            myImage.color = myImage.color.ChangedAlpha(0);
+            leftImage.GetComponentInChildren<NameSetText>().SetText("");
+        }
+
+        public void ActivateLeftName(string text)
+        {
+            Image myImage = leftImage.GetComponentInChildren<NameSetText>().GetComponent<Image>();
+            myImage.color = myImage.color.ChangedAlpha(1);
+            leftImage.GetComponentInChildren<NameSetText>().SetText(text);
+            if (specialEntryMode)
+                myImage.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ ChatMage/Assets/Game/UI/In Game/Options/InGameOptions.cs
@@ -0,0 +1,104 @@
+using CCC.Manager;
+using CCC.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class InGameOptions : WindowAnimation
+{
+    public const string SCENENAME = "InGameOptions";
+
+    [Header("Options Menu")]
+    public Button levelSelectButton;
+
+    private bool isQuitting = false;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        levelSelectButton.gameObject.SetActive(Game.instance.framework.CanGoToLevelSelect);
+    }
+
+    public void Confirm()
+    {
+        SoundManager.Save();
+        Exit();
+    }
+
+    public void Cancel()
+    {
+        SoundManager.Load();
+        Exit();
+    }
+
+    public void RestartGame()
+    {
+        if (Game.instance != null)
+            Game.instance.framework.RestartLevel();
+    }
+
+    public void BackToLevelSelect()
+    {
+        LoadingScreen.TransitionTo(LevelSelect.LevelSelection.SCENENAME, null);
+    }
+
+    public void Exit()
+    {
+        if (isQuitting) return;
+
+        isQuitting = true;
+
+        if (this != null)
+        {
+            Close(
+                delegate ()
+                {
+                    Scenes.UnloadAsync(SCENENAME);
+                    Game.instance.gameRunning.Unlock("optionsMenu");
+                    isQuitting = false;
+                }
+            );
+        }
+        else
+        {
+            Scenes.UnloadAsync(SCENENAME);
+            isQuitting = false;
+        }
+    }
+
+    public void CHEAT_Invincible()
+    {
+        Game.instance.Player.playerStats.damagable = false;
+    }
+    public void CHEAT_FastSmash()
+    {
+        SmashManager sm = Game.instance.smashManager;
+        if (sm.CurrentSmashBall != null)
+            sm.CurrentSmashBall.ForceDeath();
+        else
+            sm.DecreaseCooldown(sm.RemainingTime);
+    }
+    public void CHEAT_Win()
+    {
+        Game.instance.levelScript.Win();
+    }
+    public void CHEAT_Lose()
+    {
+        Game.instance.levelScript.Lose();
+    }
+
+    public static void Open()
+    {
+        if (Game.instance == null)
+        {
+            Debug.LogWarning("Cannot open InGameOptions if the game is not running.");
+            return;
+        }
+
+        Scenes.LoadAsync(SCENENAME, LoadSceneMode.Additive);
+        Game.instance.gameRunning.Lock("optionsMenu");
+    }
+}
