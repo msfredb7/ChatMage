d34e75ef45a52a17d91670cbdb2b4dead288cb81
Fred Bessette <msfredb7@gmail.com>
2017-12-03 04:24:13 -0500

    3 classe de scriptable object qui vont nous aider à organiser le son

--- ChatMage/Assets/Other & Unused/Fred_TestScript.cs
+++ ChatMage/Assets/Other & Unused/Fred_TestScript.cs
@@ -6,42 +6,21 @@ using FullInspector;
 
 public class Fred_TestScript : BaseBehavior
 {
-    public A variable;
-    public bool useCube;
-
-    [InspectorShowIf("useCube")]
-    public GameObject cube;
+    public AudioClip clip;
+    public AudioSource source;
+    public float volume;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.T))
         {
-            cube.transform.DOMoveY(3, 2).SetEase(Ease.OutBounce).SetDelay(1).SetLoops(-1, LoopType.Yoyo);
+            source.PlayOneShot(clip, volume);
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            source.volume = volume;
+            source.clip = clip;
+            source.Play();
         }
     }
-
-    [InspectorButton]
-    void SomeMethod()
-    {
-
-    }
-}
-
-[System.Serializable]
-public abstract class A
-{
-
-}
-
-[System.Serializable]
-public class B : A
-{
-    public int health;
-    public A a;
-}
-
-[System.Serializable]
-public class C : A
-{
-
 }
