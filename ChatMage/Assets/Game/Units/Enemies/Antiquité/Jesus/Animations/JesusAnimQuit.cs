e5a5c6973c4375dd1e81be8043b583e6e51016a5
Fred Bessette <msfredb7@gmail.com>
2017-12-15 17:55:20 -0500

    Cleanup du dossier de Brutus (Jésus)

---
+++ ChatMage/Assets/Game/Units/Enemies/Antiquité/Jesus/Animations/JesusAnimQuit.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class JesusAnimQuit : StateMachineBehaviour
+{
+    public bool throwAnim;
+    public bool pickupAnim;
+    public bool awakenAnim;
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (pickupAnim)
+        {
+            animator.GetComponent<JesusV2AnimatorV2>()._PickUpComplete();
+        }
+        else if (throwAnim)
+        {
+            animator.GetComponent<JesusV2AnimatorV2>()._ThrowComplete();
+        }
+        else if (awakenAnim)
+        {
+            animator.GetComponent<JesusV2AnimatorV2>()._AwakenComplete();
+        }
+    }
+}
---
+++ ChatMage/Assets/Plugins/CCC/Audio/SoundManagerRelay.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerRelay : MonoBehaviour
+{
+    public void PlaySFX_AudioClip(AudioClip clip)
+    {
+
+    }
+    public void PlaySFX_AudioPlayable(AudioPlayable playable)
+    {
+
+    }
+    public void PlayVoice_AudioClip(AudioClip clip)
+    {
+
+    }
+    public void PlayVoice_AudioPlayable(AudioPlayable playable)
+    {
+
+    }
+    public void PlayStaticSFX_AudioClip(AudioClip clip)
+    {
+
+    }
+    public void PlayStaticSFX_AudioPlayable(AudioPlayable playable)
+    {
+
+    }
+    public void PlayMusic_AudioClip(AudioClip clip)
+    {
+
+    }
+    public void PlayMusic_AudioPlayable(AudioPlayable playable)
+    {
+
+    }
+}
