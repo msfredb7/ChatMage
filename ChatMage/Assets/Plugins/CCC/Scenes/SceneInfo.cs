0bc23f853b0ff7835384e8f1cd6515016275f09f
Fred Gatineau <wdad>
2018-01-01 17:28:43 -0500

    Scene Info, suite

--- ChatMage/Assets/Plugins/CCC/Scenes/SceneInfo.cs
+++ ChatMage/Assets/Plugins/CCC/Scenes/SceneInfo.cs
@@ -15,11 +15,11 @@ public class SceneInfo : ScriptableObject
 #if UNITY_EDITOR
     [SerializeField]
     private SceneAsset scene;
-    public SceneAsset GetEditorScene()
+    public SceneAsset Editor_GetScene()
     {
         return scene;
     }
-    public void RefreshSceneName()
+    public void Editor_RefreshSceneName()
     {
         sceneName = scene.name;
     }
@@ -72,12 +72,149 @@ public class SceneInfoEditor : Editor
         _target = target as SceneInfo;
     }
 
+    private static void AddToBuild(string assetPath)
+    {
+        var original = EditorBuildSettings.scenes;
+        var newSettings = new EditorBuildSettingsScene[original.Length + 1];
+        Array.Copy(original, newSettings, original.Length);
+
+        EditorBuildSettingsScene sceneToAdd = new EditorBuildSettingsScene(assetPath, true);
+        newSettings[newSettings.Length - 1] = sceneToAdd;
+
+        EditorBuildSettings.scenes = newSettings;
+    }
+
+    private static void EnableInBuild(int buildIndex)
+    {
+        var scenes = EditorBuildSettings.scenes;
+        scenes[buildIndex].enabled = true;
+        EditorBuildSettings.scenes = scenes;
+    }
+
+    private static void DisableInBuild(int buildIndex)
+    {
+        var scenes = EditorBuildSettings.scenes;
+        scenes[buildIndex].enabled = false;
+        EditorBuildSettings.scenes = scenes;
+    }
+
+    private static void PutInFirstInBuild(int buildIndex)
+    {
+        var original = EditorBuildSettings.scenes;
+        var newSettings = new EditorBuildSettingsScene[original.Length];
+
+        EditorBuildSettingsScene theOne = original[buildIndex];
+
+        int u = 1;
+        for (int i = 0; i < original.Length; i++)
+        {
+            if (i != buildIndex)
+            {
+                newSettings[u] = original[i];
+                u++;
+            }
+        }
+        newSettings[0] = theOne;
+
+        EditorBuildSettings.scenes = newSettings;
+    }
+    private static void RemoveFromBuild(int buildIndex)
+    {
+        var original = EditorBuildSettings.scenes;
+        var newSettings = new EditorBuildSettingsScene[original.Length - 1];
+
+        int u = 0;
+        for (int i = 0; i < original.Length; i++)
+        {
+            if (i != buildIndex)
+            {
+                newSettings[u] = original[i];
+                u++;
+            }
+        }
+
+        EditorBuildSettings.scenes = newSettings;
+    }
+
+    /// <summary>
+    ///  -1 -> not in build   0 -> disabled in build   2 -> enabled in build
+    /// </summary>
+    private int GetBuildState(out string assetPath, out int indexInBuild)
+    {
+        SceneAsset sceneAsset = _target.Editor_GetScene();
+        assetPath = AssetDatabase.GetAssetOrScenePath(sceneAsset);
+        EditorBuildSettingsScene[] scenes = EditorBuildSettings.scenes;
+        for (int i = 0; i < scenes.Length; i++)
+        {
+            if (scenes[i].path == assetPath)
+            {
+                indexInBuild = i;
+                if (scenes[i].enabled)
+                    return 1;
+                else
+                    return 0;
+            }
+        }
+
+        indexInBuild = -1;
+        return -1;
+    }
+
     public override void OnInspectorGUI()
     {
         Color guiColor = GUI.color;
-        if (_target.GetEditorScene() != null && _target.GetEditorScene().name != _target.SceneName)
+        SceneAsset sceneAsset = _target.Editor_GetScene();
+
+        if (sceneAsset != null)
         {
-            GUI.color = new Color(1, 0.5f, 0.5f);
+            string assetPath;
+            int indexInBuild;
+
+            // -1 -> not in build   0 -> disabled in build   2 -> enabled in build
+            int buildState = GetBuildState(out assetPath, out indexInBuild);
+
+            if (buildState == -1)
+            {
+                GUI.color = new Color(0.6f, 1, 0.6f);
+                if (GUILayout.Button("ADD TO BUILD SETTINGS"))
+                {
+                    AddToBuild(assetPath);
+                }
+                GUI.color = guiColor;
+            }
+            else
+            {
+                GUI.color = new Color(1, 0.6f, 0.6f);
+                if (GUILayout.Button("REMOVE FROM BUILD SETTINGS"))
+                {
+                    RemoveFromBuild(indexInBuild);
+                }
+
+                GUI.color = guiColor;
+                EditorGUILayout.BeginHorizontal();
+                if (EditorGUILayout.Toggle("Included in build", buildState == 1) != (buildState == 1))
+                {
+                    if (buildState == 1)
+                        DisableInBuild(indexInBuild);
+                    else
+                        EnableInBuild(indexInBuild);
+                }
+
+                bool isInFirstPlace = indexInBuild == 0;
+                if (isInFirstPlace)
+                    GUI.enabled = false;
+                if (GUILayout.Button(isInFirstPlace ? "Is in first place" : "Put in first place"))
+                {
+                    PutInFirstInBuild(indexInBuild);
+                }
+                GUI.enabled = true;
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+
+        if (sceneAsset != null && sceneAsset.name != _target.SceneName)
+        {
+            GUI.color = new Color(0.6f, 1, 0.6f);
         }
         else
         {
@@ -86,7 +223,7 @@ public class SceneInfoEditor : Editor
 
         if (GUILayout.Button("Refresh Name"))
         {
-            _target.RefreshSceneName();
+            _target.Editor_RefreshSceneName();
             EditorUtility.SetDirty(_target);
         }
 
