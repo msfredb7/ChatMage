2fc97fb5423803913f452324e7debef171eb17b0
Fred Bessette <msfredb7@gmail.com>
2018-01-08 17:35:52 -0500

    Ajout de SpriteColors, c'est utile pour changer la couleur des sprites sans bouffer la performance

--- ChatMage/Assets/Other & Unused/Fred Tests/Fred_TestScript.cs
+++ ChatMage/Assets/Other & Unused/Fred Tests/Fred_TestScript.cs
@@ -19,7 +19,7 @@ public class Fred_TestScript : MonoBehaviour
         MasterManager.Sync();
         Debug.LogWarning("Hello, je suis un Fred_TestScript, ne pas m'oublier ici (" + gameObject.name + ")");
 
-
+        SceneInfo s;
         float test = v1 - v2;
     }
 
