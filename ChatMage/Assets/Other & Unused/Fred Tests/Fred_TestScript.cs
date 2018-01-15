8f7dab0fc9b62b239b1deb05b683a30304a4ea5a
Fred Bessette <msfredb7@gmail.com>
2018-01-15 16:26:49 -0500

    Hits and Slashes !!!

--- ChatMage/Assets/Other & Unused/Fred Tests/Fred_TestScript.cs
+++ ChatMage/Assets/Other & Unused/Fred Tests/Fred_TestScript.cs
@@ -9,13 +9,11 @@ using UnityEngine.Events;
 
 public class Fred_TestScript : MonoBehaviour
 {
+    public MonoBehaviour someOtherScript;
+
     void Start()
     {
-        PersistentLoader.LoadIfNotLoaded(
-            ()=>
-            {
-                Debug.LogWarning("Hello, je suis un Fred_TestScript, ne pas m'oublier ici (" + gameObject.name + ")");
-            });
+        Debug.LogWarning("Hello, je suis un Fred_TestScript, ne pas m'oublier ici (" + gameObject.name + ")");
     }
 
     void Update()
