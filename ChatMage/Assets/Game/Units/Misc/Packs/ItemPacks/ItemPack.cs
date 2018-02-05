05dca6afc0b2fafd66771bfa2d515d55a9c63775
Xeal <alexandre-blanchet94@hotmail.com>
2018-02-05 15:32:53 -0500

    Ajout du code dans Item packs pour que se soit facile apres de trigger l'animation dans le UI

--- ChatMage/Assets/Game/Units/Misc/Packs/ItemPacks/ItemPack.cs
+++ ChatMage/Assets/Game/Units/Misc/Packs/ItemPacks/ItemPack.cs
@@ -7,6 +7,10 @@ public class ItemPack : Pack
     [System.NonSerialized]
     public Item itemReference;
 
+    public DataSaver firstPickupSave;
+
+    private string firstPickUpKey = "FPUK";
+
     public void SetItem(Item itemReference)
     {
         this.itemReference = itemReference;
@@ -17,6 +21,21 @@ public class ItemPack : Pack
         PlayerController player = Game.Instance != null ? Game.Instance.Player : null;
 
         if (itemReference != null && player != null)
+        {
+            // Equip item
             player.playerItems.Equip(itemReference);
+            // First Time Its Equiped ?
+            string key = firstPickUpKey + itemReference.name;
+            // Save Exist doesn't exist so its the first time
+            if (!firstPickupSave.ContainsBool(key))
+            {
+                // Create the save
+                firstPickupSave.SetBool(key, true);
+                firstPickupSave.SaveAsync();
+
+                // Trigger UI Anim
+                //Game.Instance.ui.
+            } // else it isn't the first time so its ok
+        }
     }
 }
