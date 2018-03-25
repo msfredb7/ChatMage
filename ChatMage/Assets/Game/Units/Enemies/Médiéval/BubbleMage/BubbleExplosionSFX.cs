991c1ac162a94e39b63b8990308ac036e4cc5587
Benoit Bergeron <fivocx@gmail.com>
2018-03-25 18:59:03 -0400

    Son du bubble mage

---
+++ ChatMage/Assets/Game/Units/Enemies/Médiéval/BubbleMage/BubbleExplosionSFX.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleExplosionSFX : MonoBehaviour {
+
+    public AudioPlayable bubbleExplosion;
+
+	public void PlaySound()
+    {
+        DefaultAudioSources.PlaySFX(bubbleExplosion);
+    }
+}
