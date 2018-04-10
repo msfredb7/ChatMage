8b417ef4310f3f00fabf1a176d519b52fcfac8df
Xeal <alexandre-blanchet94@hotmail.com>
2018-04-09 20:35:52 -0400

    merge 2

---
+++ ChatMage/Assets/Game/UI/Menus/EndlessExplication/DisplayExplication.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayExplication : MonoBehaviour {
+
+    public SceneInfo explicationScene;
+    public DataSaver endlessTutorialSaver;
+
+    private string alreadyDisplayedKey = "ExplicationDisplayed";
+
+    void Start()
+    {
+        if (endlessTutorialSaver.GetInt(alreadyDisplayedKey, 0) == 0)
+            Display();
+    }
+
+	public void Display()
+    {
+        endlessTutorialSaver.SetInt(alreadyDisplayedKey, 1);
+        Scenes.Load(explicationScene);
+    }
+}
