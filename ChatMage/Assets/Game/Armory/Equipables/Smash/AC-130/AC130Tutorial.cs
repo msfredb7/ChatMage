d217540b73c45ebe66736e96cc5ee8d02ce3f709
Xeal <alexandre-blanchet94@hotmail.com>
2018-04-09 22:27:17 -0400

    fix ac130

---
+++ ChatMage/Assets/Game/Armory/Equipables/Smash/AC-130/AC130Tutorial.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AC130Tutorial : MonoBehaviour {
+
+    public Text text;
+
+	void Start ()
+    {
+        if (Application.isMobilePlatform)
+        {
+            text.text = "TAP YOUR SCREEN";
+        } else
+        {
+            text.text = "CLICK YOUR SCREEN";
+        }
+    }
+}
