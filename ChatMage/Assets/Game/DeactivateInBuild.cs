0a414ec21e90bf8a10684cb9765fd5b60b11c0ca
Frédéric Bessette <msfredb7@gmail.com>
2018-04-10 21:13:04 -0400

    Ajoute de 'DeactivateInBuild' pour désactivé les boutons debugs dans les builds

---
+++ ChatMage/Assets/Game/DeactivateInBuild.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateInBuild : MonoBehaviour
+{
+    void Awake()
+    {
+        if (!Application.isEditor)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
