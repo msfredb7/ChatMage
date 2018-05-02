907ce158f01f5a17e5b7f47e4c9a3b940c37bf7c
Frédéric Bessette <msfredb7@gmail.com>
2018-05-02 15:45:55 -0400

    5:4 screen ratio fixes

--- ChatMage/Assets/Game/Armory/Equipables/Smash/AC-130/AC130CrossairAnim.cs
+++ ChatMage/Assets/Game/Armory/Equipables/Smash/AC-130/AC130CrossairAnim.cs
@@ -24,13 +24,14 @@ public class AC130CrossairAnim : MonoBehaviour
     public void Shoot(Vector2 pixelPosition, float reloadDuration)
     {
         //On trouve la position
-        Vector2 scaling = new Vector2(canvasScaler.referenceResolution.x / Screen.width, canvasScaler.referenceResolution.y / Screen.height);
-        Vector2 anchoredTouchPosition = new Vector2(pixelPosition.x * scaling.x, pixelPosition.y * scaling.y);
+        //Vector2 scaling = new Vector2(canvasScaler.referenceResolution.x / Screen.width, canvasScaler.referenceResolution.y / Screen.height);
+        //Vector2 anchoredTouchPosition = new Vector2(pixelPosition.x * scaling.x, pixelPosition.y * scaling.y);
 
 
         //Wtf unity.
-        container.anchoredPosition = Vector2.one;
-        container.anchoredPosition = anchoredTouchPosition;
+        //container.anchoredPosition = Vector2.one;
+        //container.anchoredPosition = anchoredTouchPosition;
+        container.position = pixelPosition;
 
         group.DOKill();
         group.alpha = 1;
